38b2c969dc1c93da5b312f61d786731a24c667d2
Lesley Cordero <lc2958@columbia.edu>
2017-05-06 15:00:23 -0400

    Update boba.py

--- NYC Boba/boba.py
+++ NYC Boba/boba.py
@@ -12,7 +12,7 @@ from geojsonio import display
 class BubbleTea(object):
 
 	# authentication initialized
-	gmaps = googlemaps.Client(key='your_key')
+	gmaps = googlemaps.Client(key='api_key_here')
 
 	def __init__(self, filename):
 		# initalizes csv with list of bubble tea places to dataframe
@@ -45,12 +45,46 @@ class BubbleTea(object):
 		self.boba['Distance'] = [user_loc.distance(Point(xy)) for xy in zip(self.boba.Longitude, self.boba.Lat)]
 	    	# grabs the three smallest distances
 		boba_list = self.boba.nsmallest(3, 'Distance').set_index('Name') # sets index to name
-	    	temp = (": " + boba_list['Address']).rename_axis(None).__repr__() 
-	    	return(temp.rsplit('\n', 1)[0]) # formatted to string
+		temp = (": " + boba_list['Address']).rename_axis(None).__repr__()
+		return(temp.rsplit('\n', 1)[0]) # formatted to string
 
 	# still not written
-	def update(self):
-		self.boba = self.boba['Name'].where(self.boba['Lat'] == float('nan'))
+	def add(self, name, addr, rest):
+		#self.boba = self.boba['Name'].where(self.boba['Lat'] == float('nan'))
+		obj = geocoder.google(addr)
+		df2 = pd.DataFrame({'Name': [name],
+			'Address': [addr],
+			'Restaurant': [rest],
+			'Lat': [obj.lat],
+			'Longitude': [obj.lng],
+			'Coordinates': [Point(obj.lng, obj.lat)]}, index=[len(self.boba)])
+		#buff_list = [name, addr, rest, int(obj.lat), int(obj.lng), Point(obj.lng, obj.lat)]
+		#self.boba.append([buff_list])
+		self.boba = pd.concat([self.boba, df2])
+		return(self.boba)
+
+	def visualize(self):
+		print(self.boba.to_json())
+ 
 
 if __name__ == "__main__":
-	boba1 = BubbleTea("./sample.csv")
+	buff = True
+	b1 = BubbleTea("./boba_final.csv")
+
+	while buff:
+	    bname = input("Boba Place Name:")
+	    baddress = input("Boba Place Address:")
+	    brestaurant = input("Is this place a restaurant? Y/N")
+	    update_df = b1.add(bname, baddress, brestaurant)
+	    #update_df.to_csv("./boba_final.csv")
+	    ask = user("Want to add a place? Y/N ")
+		if ask == "N":
+			buff = False
+	    	b1.visualize()
+	print(update_df)
+
+
+
+
+
+	
